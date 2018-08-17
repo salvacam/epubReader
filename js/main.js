0c6c0d81ff7f8a66db33bd7b103e0fbc17722a74
salvacam <salvacams@gmail.com>
2018-08-17 17:35:37 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+  	app.init();
+});
+
+var app = {  
+  	note: document.getElementById('note'),
+  	clearNotes: document.getElementById('clearNotes'),
+  	modalClear: document.getElementById('modalClear'),
+  
+  	init: function() {  		
+  		app.note.addEventListener('keyup',
+  			() => { setTimeout(function(){ localStorage.setItem("_note", app.note.value); }, 500) }
+		);
+		
+  		if(localStorage.getItem("_note") && localStorage.getItem("_note")!=''){
+    		var noteItem = localStorage.getItem("_note");
+    		app.note.value = noteItem;
+  		}
+
+	  	app.clearNotes.addEventListener('click', () => {
+	  		app.modalClear.classList.remove('hide');
+
+
+  			document.getElementById('okClear').addEventListener('click', () => {  				
+				app.modalClear.classList.add('hide');
+		    	app.note.value = '';
+		    	localStorage.setItem("_note", '');
+				document.getElementById('okClear').removeEventListener('click', ()=> {});
+  			});
+  			document.getElementById('closeClear').addEventListener('click', () => {  				
+				app.modalClear.classList.add('hide');
+				document.getElementById('closeClear').removeEventListener('click', ()=> {});
+  			});
+
+		    app.note.focus();
+
+		    if ('serviceWorker' in navigator) {
+	      		navigator.serviceWorker
+	        		.register('service-worker.js')
+	        		.then(function() {
+	          		//console.log('Service Worker Registered');
+	        	});
+    		}
+		});
+  	}
+};
---
+++ service-worker.js
@@ -0,0 +1,48 @@
+var cacheName = 'notepad-v0.1.01';
+
+var filesToCache = [
+  './',
+  './index.html',
+  './js/main.js',
+  './img/icon.png',
+  './img/icon-128x128.png',
+  './img/icon-144x144.png',
+  './img/icon-192x192.png',
+  './img/icon-48x48.png',
+  './img/icon-512x512.png',
+  './img/icon-96x96.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install_');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate_');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key.startsWith('polen-')){
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
